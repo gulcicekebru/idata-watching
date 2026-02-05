d2df3ba056120ca9c33ff485ffde077a53036b1d
Ebru Türkmen <52013451+gulcicekebru@users.noreply.github.com>
2026-02-05 17:20:51 +0300

    Update telegram_notifier.py
    fixing codes

--- src/telegram_notifier.py
+++ src/telegram_notifier.py
@@ -1,18 +1,21 @@
+import asyncio
 import yaml
 from telegram import Bot
 
-# config oku
+# settings.yaml oku
 with open("config/settings.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 BOT_TOKEN = config["telegram"]["token"]
 CHAT_ID = config["telegram"]["chat_id"]
 
-bot = Bot(token=BOT_TOKEN)
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="🤖 Merhaba! iDATA watcher botu ayakta."
+    )
+    print("Mesaj gerçekten gönderildi")
 
-bot.send_message(
-    chat_id=CHAT_ID,
-    text="🤖 Merhaba! iDATA watcher botu ayakta."
-)
-
-print("Mesaj gönderildi")
+if __name__ == "__main__":
+    asyncio.run(main())
