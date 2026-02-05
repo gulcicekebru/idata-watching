00a0b3283a7ec11e3dd785a86eba83d341c85dde
Ebru Türkmen <52013451+gulcicekebru@users.noreply.github.com>
2026-02-05 17:16:21 +0300

    Update get_chat_id.py
    adding await

--- src/get_chat_id.py
+++ src/get_chat_id.py
@@ -2,19 +2,20 @@ import asyncio
 import yaml
 from telegram import Bot
 
-# settings.yaml oku
+# config oku
 with open("config/settings.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 BOT_TOKEN = config["telegram"]["token"]
+CHAT_ID = config["telegram"]["chat_id"]
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
-    updates = await bot.get_updates()
-
-    for update in updates:
-        if update.message:
-            print("Chat ID:", update.message.chat_id)
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="🤖 Merhaba! iDATA watcher botu ayakta."
+    )
+    print("Mesaj gerçekten gönderildi")
 
 if __name__ == "__main__":
     asyncio.run(main())
